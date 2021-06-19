1f9538641d1157005fd8d64005142460c191c065
胡加一路 <hujiayilu@outlook.com>
2021-06-19 23:23:22 +0800

    add DownloadFileWithFormat

--- helpers/networkHelper.go
+++ helpers/networkHelper.go
@@ -264,4 +264,30 @@ func (networkHelper *NetworkHelper) DownloadFile(url string, fb func(string) err
 }
 
 
+func (networkHelper *NetworkHelper) DownloadFileWithFormat(url,format string, fb func(string) error) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// 创建一个文件用于保存
+	var strHelper StrHelper
+	tempPath := "/tmp/download/" + strHelper.GetRandomString(10) + "." + format
+	out, err := os.Create(tempPath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// 然后将响应流和文件流对接起来
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	return fb(tempPath)
+}
+
+
 
