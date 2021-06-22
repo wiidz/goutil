e688e923d0be2df1a007da4b9a5e92e4458a5dcf
胡加一路 <hujiayilu@outlook.com>
2021-06-22 11:47:10 +0800

    add config mng

---
+++ mngs/configMng/main.go
@@ -0,0 +1,84 @@
+package configMng
+
+import (
+	"fmt"
+	"github.com/wiidz/goutil/helpers"
+)
+
+const (
+	ConfigPath           = "./configs/" // 路径写死
+	DevPath = "dev/"
+	PdcPath = "pdc/"
+	AppConfigFileName    = "app.json"
+	WechatConfigFileName = "wechat.json"
+	MysqlConfigFileName  = "mysql.json"
+	EsConfigFileName     = "es.json"
+	RedisConfigFileName  = "redis.json"
+	OssConfigFileName    = "oss.json"
+)
+
+var appConfig = AppConfig{}
+var osHelper = helpers.OsHelper{}
+
+type ConfigMng struct{}
+
+func init() {
+	buf := osHelper.GetFileBuf(ConfigPath + AppConfigFileName)
+	_ = appConfig.UnmarshalJSON(buf)
+}
+
+// 获取指定目录里的配置文件
+func getTargetDir() string {
+	var dir string
+	if appConfig.Debug == false {
+		dir = PdcPath
+	} else {
+		dir = DevPath
+	}
+	return ConfigPath + dir
+}
+
+func getFileBuf(fileName string) []byte {
+	return osHelper.GetFileBuf(getTargetDir() + fileName)
+}
+
+func (*ConfigMng) GetHttpPort() string {
+	return appConfig.HttpPort
+}
+
+func (*ConfigMng) GetMysql() MysqlConfig {
+	buf := getFileBuf(MysqlConfigFileName)
+	mysqlConfig := MysqlConfig{}
+	_ = mysqlConfig.UnmarshalJSON(buf)
+	return mysqlConfig
+}
+
+func (*ConfigMng) GetRedis() RedisConfig {
+	buf := getFileBuf(RedisConfigFileName)
+	redisConfig := RedisConfig{}
+	_ = redisConfig.UnmarshalJSON(buf)
+	return redisConfig
+}
+
+func (*ConfigMng) GetWechat() WechatConfig {
+	buf := getFileBuf(WechatConfigFileName)
+	wechatConfig := WechatConfig{}
+	_ = wechatConfig.UnmarshalJSON(buf)
+	return wechatConfig
+}
+
+func (*ConfigMng) GetOss() OssConfig {
+	buf := getFileBuf(OssConfigFileName)
+	ossConfig := OssConfig{}
+	_ = ossConfig.UnmarshalJSON(buf)
+	fmt.Println("ossConfig", ossConfig)
+	return ossConfig
+}
+
+func (*ConfigMng) GetEs() EsConfig {
+	buf := getFileBuf(EsConfigFileName)
+	var esConfig EsConfig
+	_ = esConfig.UnmarshalJSON(buf)
+	fmt.Println("esConfig", esConfig)
+	return esConfig
+}
---
+++ mngs/configMng/structs.go
@@ -0,0 +1,62 @@
+package configMng
+
+//easyjson:json
+type AppConfig struct {
+	Debug    bool   `json:"debug"`
+	HttpPort string `json:"http_port"`
+}
+
+//easyjson:json
+type MysqlConfig struct {
+	Host      string `json:"host"`
+	Port      string `json:"port"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Collation string `json:"collation"`
+	DbName    string `json:"db_name"`
+	Charset   string `json:"charset"`
+}
+
+//easyjson:json
+type EsConfig struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+//easyjson:json
+type OssConfig struct {
+	AccessKeyID     string `json:"access_key_id"`
+	AccessKeySecret string `json:"access_key_secret"`
+	EndPoint        string `json:"end_point"`
+	BucketName      string `json:"bucket_name"`
+	UploadPath      string `json:"upload_path"`
+	Host            string `json:"host"`
+	CallBackUrl     string `json:"call_back_url"` //为上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+	ExpireTime      int64  `json:"expire_time"`   // 上传策略Policy的失效时间，单位为秒。
+}
+
+//easyjson:json
+type RedisConfig struct {
+	IP          string `json:"ip"`
+	Port        string `json:"port"`
+	Password    string `json:"password"`
+	IdleTimeout int    `json:"idle_timeout"`
+	Database    int    `json:"datebase"`
+	MaxActive   int    `json:"max_active"`
+	MaxIdle     int    `json:"max_idle"`
+}
+
+//easyjson:json
+type WechatConfig struct {
+	AppID       string `json:"app_id"`
+	AppSecret   string `json:"app_secret"`
+	GrantType   string `json:"grant_type"`
+	PayKey      string `json:"pay_key"`
+	MechID      string `json:"mech_id"`
+	NotifyUrl   string `json:"notify_url"`
+	RefundUrl   string `json:"refund_url"`
+	CertPath    string `json:"cert_path"`
+	CertKeyPath string `json:"cert_key_path"`
+}
