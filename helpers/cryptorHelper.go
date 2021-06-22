7f4944404245ca808a8a767b1b8d7b69100cf01a
胡加一路 <hujiayilu@outlook.com>
2021-06-22 12:54:38 +0800

    add cryptor

--- helpers/cryptorHelper.go
+++ helpers/cryptorHelper.go
@@ -1,1 +1,15 @@
 package helpers
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+)
+
+type CryptorHelper struct{}
+
+func (*CryptorHelper) MD5Encrypt(raw string) string {
+	w := md5.New()
+	io.WriteString(w, raw)
+	return fmt.Sprintf("%x", w.Sum(nil))
+}
