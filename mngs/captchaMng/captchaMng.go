ad292f08adbc0b1f426d34fbe2d2a19422b19f69
胡加一路 <hujiayilu@outlook.com>
2021-06-22 23:01:20 +0800

    update captcha

--- mngs/captchaMng/captchaMng.go
+++ mngs/captchaMng/captchaMng.go
@@ -8,22 +8,6 @@ import (
 
 // 目前是单机部署版本，后续改redis
 
-var captcha *cp.Captcha
-
-const (
-	height = 43
-	width  = 200
-	length = 4
-)
-
-//创建字符串验证码实例
-func init() {
-	driver := cp.NewDriverString(height, width, 0, cp.OptionShowHollowLine,
-		length, cp.TxtSimpleCharaters, &color.RGBA{254, 254, 254, 254}, []string{"Flim-Flam.ttf"})
-	cape := cp.NewCaptcha(driver, cp.DefaultMemStore)
-	captcha = cape
-}
-
 //验证是否有效
 func VerifyCaptcha(id, answer string) bool {
 	get := cp.DefaultMemStore.Get(id, false)
@@ -37,6 +21,10 @@ func VerifyCaptcha(id, answer string) bool {
 }
 
 //生成base64
-func GenerateCaptcha() (id, b64s string, err error) {
+func GenerateCaptcha(width,height,noiseCount,length int) (id, b64s string, err error) {
+	driver := cp.NewDriverString(height, width, noiseCount, cp.OptionShowHollowLine,
+		length, cp.TxtSimpleCharaters, &color.RGBA{254, 254, 254, 254}, []string{"Flim-Flam.ttf"})
+	captcha := cp.NewCaptcha(driver, cp.DefaultMemStore)
+
 	return captcha.Generate()
 }
