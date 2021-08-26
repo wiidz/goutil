d8b3bff0d9d736538c15d06ece83ed5878e54dbf
hujiayilu@outlook.com <hujiayilu@outlook.com>
2021-08-26 17:52:52 +0800

    增加了timeHelper的几个方法

--- helpers/timeHelper/timeHelper.go
+++ helpers/timeHelper/timeHelper.go
@@ -223,3 +223,126 @@ func (tm MyJsonTime) Format2Time() time.Time {
 func GetCST8Now() time.Time{
 	return time.Now().UTC().Add(8 * time.Hour)
 }
+
+
+
+// GetFirstDateOfWeek 获取本周周一的日期
+func GetFirstDateOfWeek(target time.Time) (weekStartDate time.Time) {
+
+	offset := int(time.Monday - target.Weekday())
+	if offset > 0 {
+		offset = -6
+	}
+
+	return time.Date(target.Year(), target.Month(), target.Day(), 0, 0, 0, 0, time.Local).AddDate(0, 0, offset)
+}
+
+
+// GetLastDateOfWeek 获取本周周日的日期
+func GetLastDateOfWeek(target time.Time) (weekStartDate time.Time) {
+
+	offset := int(time.Saturday - target.Weekday())
+	if offset > 6 {
+		offset = -1
+	}
+
+	return time.Date(target.Year(), target.Month(), target.Day(), 0, 0, 0, 0, time.Local).AddDate(0, 0, offset)
+}
+
+
+// GetMonthDayStr 获得当前月的初始和结束日期
+func GetMonthDayStr() (string, string) {
+	now := time.Now()
+	currentYear, currentMonth, _ := now.Date()
+	currentLocation := now.Location()
+
+	firstOfMonth := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
+	lastOfMonth := firstOfMonth.AddDate(0, 1, -1)
+	f := firstOfMonth.Unix()
+	l := lastOfMonth.Unix()
+	return time.Unix(f, 0).Format("2006-01-02") + " 00:00:00", time.Unix(l, 0).Format("2006-01-02") + " 23:59:59"
+}
+
+
+// GetWeekDayStr 获得当前周的初始和结束日期
+func GetWeekDayStr() (string, string) {
+	now := time.Now()
+	offset := int(time.Monday - now.Weekday())
+	//周日做特殊判断 因为time.Monday = 0
+	if offset > 0 {
+		offset = -6
+	}
+
+	lastOffset := int(time.Saturday - now.Weekday())
+	//周日做特殊判断 因为time.Monday = 0
+	if lastOffset == 6 {
+		lastOffset = -1
+	}
+
+	firstOfWeek := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).AddDate(0, 0, offset)
+	lastOfWeeK := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).AddDate(0, 0, lastOffset+1)
+	f := firstOfWeek.Unix()
+	l := lastOfWeeK.Unix()
+	return time.Unix(f, 0).Format("2006-01-02") + " 00:00:00", time.Unix(l, 0).Format("2006-01-02") + " 23:59:59"
+}
+
+
+// GetQuarterDayStr 获得当前季度的初始和结束日期
+func GetQuarterDayStr() (string, string) {
+	year := time.Now().Format("2006")
+	month := int(time.Now().Month())
+	var firstOfQuarter string
+	var lastOfQuarter string
+	if month >= 1 && month <= 3 {
+		//1月1号
+		firstOfQuarter = year + "-01-01 00:00:00"
+		lastOfQuarter = year + "-03-31 23:59:59"
+	} else if month >= 4 && month <= 6 {
+		firstOfQuarter = year + "-04-01 00:00:00"
+		lastOfQuarter = year + "-06-30 23:59:59"
+	} else if month >= 7 && month <= 9 {
+		firstOfQuarter = year + "-07-01 00:00:00"
+		lastOfQuarter = year + "-09-30 23:59:59"
+	} else {
+		firstOfQuarter = year + "-10-01 00:00:00"
+		lastOfQuarter = year + "-12-31 23:59:59"
+	}
+	return firstOfQuarter, lastOfQuarter
+}
+
+// GetBetweenDateStrs 根据开始日期和结束日期计算出时间段内所有日期
+// 参数为日期格式，如：2020-01-01
+func GetBetweenDateStrs(startDate, endDate string) []string {
+	d := []string{}
+	timeFormatTpl := "2006-01-02 15:04:05"
+	if len(timeFormatTpl) != len(startDate) {
+		timeFormatTpl = timeFormatTpl[0:len(startDate)]
+	}
+	date, err := time.Parse(timeFormatTpl, startDate)
+	if err != nil {
+		// 时间解析，异常
+		return d
+	}
+	date2, err := time.Parse(timeFormatTpl, endDate)
+	if err != nil {
+		// 时间解析，异常
+		return d
+	}
+	if date2.Before(date) {
+		// 如果结束时间小于开始时间，异常
+		return d
+	}
+	// 输出日期格式固定
+	timeFormatTpl = "2006-01-02"
+	date2Str := date2.Format(timeFormatTpl)
+	d = append(d, date.Format(timeFormatTpl))
+	for {
+		date = date.AddDate(0, 0, 1)
+		dateStr := date.Format(timeFormatTpl)
+		d = append(d, dateStr)
+		if dateStr == date2Str {
+			break
+		}
+	}
+	return d
+}
