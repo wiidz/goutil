3a82ec996e848fc85134bf7bed1b8716a63b7458
hujiayilu@outlook.com <hujiayilu@outlook.com>
2021-08-30 14:09:51 +0800

    调jwtmng

--- mngs/jwtMng/jwtMng.go
+++ mngs/jwtMng/jwtMng.go
@@ -9,6 +9,7 @@ import (
 	"github.com/wiidz/goutil/helpers/typeHelper"
 	"github.com/wiidz/goutil/mngs/redisMng"
 	"golang.org/x/xerrors"
+	"log"
 	"reflect"
 	"strings"
 	"time"
@@ -209,6 +210,14 @@ func (mng *JwtMng) CompareJwtCache(appID, userID uint64, token string) error {
 // Tips：此方法试用于前后端非同表的项目，判断是否是前端（tokenData里是否有jwtMng约定的主键）
 func (mng *JwtMng) IsPkSet()  bool {
 	immutable := reflect.ValueOf(mng.TokenStruct)
+
+	log.Println("aa",immutable.IsValid())
+	log.Println("aa",immutable.IsZero())
+
+
+	if immutable.IsValid() == false{
+		return false
+	}
 
 	temp := immutable.Elem().FieldByName(mng.IdentifyKey)
 
