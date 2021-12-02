dc6c7f4b2e7bb57239ae7b36b381ddc3500d2988
hujiayilu@outlook.com <hujiayilu@outlook.com>
2021-12-02 18:48:37 +0800

    test

--- mngs/lbsMng/lbsMng.go
+++ mngs/lbsMng/lbsMng.go
@@ -5,7 +5,8 @@ import (
 	"github.com/wiidz/goutil/structs/configStruct"
 )
 
-const Domain = "https://regeo.market.alicloudapi.com/v3"
+const ReGeoURL = "https://regeo.market.alicloudapi.com/v3/geocode/regeo"
+const GeoURL = "https://geo.market.alicloudapi.com/v3/geocode/geo"
 
 // 以下方法均为高德地图在阿里云市场中提供的服务
 
@@ -23,8 +24,27 @@ func GetLbsMng(config *configStruct.AliApiConfig)*LbsMng{
 // ReGeo : 逆地理编码(将经纬度转换为详细结构化的地址，且返回附近周边的POI、AOI信息)
 func (mng *LbsMng)ReGeo(longitude,latitude string)(*ReGeoData,error){
 
-	resStr, _, _, err := networkHelper.RequestJsonWithStruct(networkHelper.Get, Domain+"/geocode/regeo", map[string]interface{}{
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkHelper.Get, ReGeoURL, map[string]interface{}{
 		"location": longitude + "," + latitude,
+	}, map[string]string{
+		"Authorization": "APPCODE " + mng.Config.AppCode,
+	},&ReGeoData{})
+
+	if err != nil {
+		return nil,err
+	}
+
+	//return resStr.(*ReGeoRes).ReGeoCode,nil
+	return res.(*ReGeoData),nil
+}
+
+
+// Geo : 地理编码(将详细的结构化地址转换为高德经纬度坐标。且支持对地标性名胜景区、建筑物名称解析为高德经纬度坐标)
+// Tips：举例，北京市朝阳区阜通东大街6号转换后经纬度：116.480881,39.989410 地
+func (mng *LbsMng)Geo(address string)(*ReGeoData,error){
+
+	resStr, _, _, err := networkHelper.RequestJsonWithStruct(networkHelper.Get, GeoURL, map[string]interface{}{
+		"address": address,
 	}, map[string]string{
 		"Authorization": "APPCODE " + mng.Config.AppCode,
 	},&ReGeoRes{})
@@ -36,6 +56,3 @@ func (mng *LbsMng)ReGeo(longitude,latitude string)(*ReGeoData,error){
 	return resStr.(*ReGeoRes).ReGeoCode,nil
 }
 
-
-// Geo : 地理编码(将详细的结构化地址转换为高德经纬度坐标。且支持对地标性名胜景区、建筑物名称解析为高德经纬度坐标)
-// Tips：举例，北京市朝阳区阜通东大街6号转换后经纬度：116.480881,39.989410 地
--- mngs/lbsMng/lbsMng_test.go
+++ mngs/lbsMng/lbsMng_test.go
@@ -6,15 +6,17 @@ import (
 	"testing"
 )
 
-func TestGetLbsMng(t *testing.T){
+func TestReGeo(t *testing.T){
 	t.Log("start")
 	lbsM := lbsMng.GetLbsMng(&configStruct.AliApiConfig{
-		AppCode:   "",
+		AppCode:   "7191c7623e4043388c6a7d06f6589997",
 		AppID:     "",
-		AppSecret: "",
+		AppSecret: "7191c7623e4043388c6a7d06f6589997",
 	})
-	latitude := "30.319352"
-	longitude := "120.388651"
+	//latitude := "30.319352"
+	//longitude := "120.388651"
+	latitude := "39.990056"
+	longitude := "116.482005"
 	res,err :=  lbsM.ReGeo(longitude,latitude)
 
 	if err != nil {
@@ -23,3 +25,34 @@ func TestGetLbsMng(t *testing.T){
 
 	t.Log("res", res)
 }
+
+
+
+//func TestGeo(t *testing.T){
+//	t.Log("start")
+//	lbsM := lbsMng.GetLbsMng(&configStruct.AliApiConfig{
+//		AppCode:   "7191c7623e4043388c6a7d06f6589997",
+//		AppID:     "",
+//		AppSecret: "7191c7623e4043388c6a7d06f6589997",
+//	})
+//	res,err :=  lbsM.Geo("北京市朝阳区阜通东大街")
+//
+//	if err != nil {
+//		t.Log("err", err)
+//	}
+//
+//	t.Log("res", res)
+//}
+
+
+
+//func TestExpress(t *testing.T){
+//
+//	resStr, _, _, err := networkHelper.RequestJson(networkHelper.Get, "https://wuliu.market.alicloudapi.com/kdi", map[string]interface{}{
+//		"no": "75831566556911",
+//	}, map[string]string{
+//		"Authorization": "APPCODE 7191c7623e4043388c6a7d06f6589997",
+//	})
+//	t.Log("resStr", resStr)
+//	t.Log("err", err)
+//}
