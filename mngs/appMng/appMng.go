6be5ffa97b980a592d93d28a3958c790ed89992d
hujiayilu@outlook.com <hujiayilu@outlook.com>
2021-12-15 13:35:18 +0800

    explode get row

--- mngs/appMng/appMng.go
+++ mngs/appMng/appMng.go
@@ -110,7 +110,7 @@ func (mng *AppMng) SetBaseConfig(dbName string, tableName string) (config *confi
 
 // getLocationConfig : 获取时区设置
 func getLocationConfig(rows []*DbSettingRow) (location *time.Location) {
-	timeZone := getRow(rows, "time_zone", "", "", "Asia/Shanghai").Value
+	timeZone := GetValueFromRow(rows, "time_zone", "", "", "Asia/Shanghai").Value
 	if timeZone == "" {
 		timeZone = "Asia/Shanghai" // 默认东八区
 	}
@@ -120,119 +120,119 @@ func getLocationConfig(rows []*DbSettingRow) (location *time.Location) {
 
 //func getMysqlConfig(rows []*DbSettingRow) *configStruct.MysqlConfig {
 //	return &configStruct.MysqlConfig{
-//		Host:     getRow(rows, "mysql", "host", "").Value,
-//		Username: getRow(rows, "mysql", "username", "").Value,
-//		Password: getRow(rows, "mysql", "password", "").Value,
-//		Port:     getRow(rows, "mysql", "port", "").Value,
+//		Host:     GetValueFromRow(rows, "mysql", "host", "").Value,
+//		Username: GetValueFromRow(rows, "mysql", "username", "").Value,
+//		Password: GetValueFromRow(rows, "mysql", "password", "").Value,
+//		Port:     GetValueFromRow(rows, "mysql", "port", "").Value,
 //	}
 //}
 
 // getWechatMiniConfig : 获取微信小程序设置
 func getWechatMiniConfig(rows []*DbSettingRow) *configStruct.WechatMiniConfig {
 	return &configStruct.WechatMiniConfig{
-		AppID:     getRow(rows, "wechat", "mini", "app_id", "").Value,
-		AppSecret: getRow(rows, "wechat", "mini", "app_secret", "").Value,
+		AppID:     GetValueFromRow(rows, "wechat", "mini", "app_id", "").Value,
+		AppSecret: GetValueFromRow(rows, "wechat", "mini", "app_secret", "").Value,
 	}
 }
 
 func getWechatOaConfig(rows []*DbSettingRow) *configStruct.WechatOaConfig {
 	return &configStruct.WechatOaConfig{
-		AppID:     getRow(rows, "wechat", "oa", "app_id", "").Value,
-		AppSecret: getRow(rows, "wechat", "oa", "app_secret", "").Value,
+		AppID:     GetValueFromRow(rows, "wechat", "oa", "app_id", "").Value,
+		AppSecret: GetValueFromRow(rows, "wechat", "oa", "app_secret", "").Value,
 	}
 }
 
 func getWechatOpenConfig(rows []*DbSettingRow) *configStruct.WechatOpenConfig {
 	return &configStruct.WechatOpenConfig{
-		AppID:     getRow(rows, "wechat", "open", "app_id", "").Value,
-		AppSecret: getRow(rows, "wechat", "open", "app_secret", "").Value,
+		AppID:     GetValueFromRow(rows, "wechat", "open", "app_id", "").Value,
+		AppSecret: GetValueFromRow(rows, "wechat", "open", "app_secret", "").Value,
 	}
 }
 func getWechatPayConfig(rows []*DbSettingRow) *configStruct.WechatPayConfig {
 	return &configStruct.WechatPayConfig{
-		AppID:           getRow(rows, "wechat", "pay", "app_id", "").Value,
-		ApiKey:          getRow(rows, "wechat", "pay", "api_key", "").Value,
-		MchID:           getRow(rows, "wechat", "pay", "mch_id", "").Value,
-		CertURI:         getRow(rows, "wechat", "pay", "cert_uri", "").Value,
-		KeyURI:          getRow(rows, "wechat", "pay", "key_uri", "").Value,
-		CertContent:     getRow(rows, "wechat", "pay", "cert_content", "").Value,
-		NotifyURL:       getRow(rows, "wechat", "pay", "notify_url", "").Value,
-		RefundNotifyURL: getRow(rows, "wechat", "pay", "refund_notify_url", "").Value,
-		IsProd:          getRow(rows, "wechat", "pay", "is_prod", "0").Value == "1", // 0=调试，1=生产
+		AppID:           GetValueFromRow(rows, "wechat", "pay", "app_id", "").Value,
+		ApiKey:          GetValueFromRow(rows, "wechat", "pay", "api_key", "").Value,
+		MchID:           GetValueFromRow(rows, "wechat", "pay", "mch_id", "").Value,
+		CertURI:         GetValueFromRow(rows, "wechat", "pay", "cert_uri", "").Value,
+		KeyURI:          GetValueFromRow(rows, "wechat", "pay", "key_uri", "").Value,
+		CertContent:     GetValueFromRow(rows, "wechat", "pay", "cert_content", "").Value,
+		NotifyURL:       GetValueFromRow(rows, "wechat", "pay", "notify_url", "").Value,
+		RefundNotifyURL: GetValueFromRow(rows, "wechat", "pay", "refund_notify_url", "").Value,
+		IsProd:          GetValueFromRow(rows, "wechat", "pay", "is_prod", "0").Value == "1", // 0=调试，1=生产
 	}
 }
 
 func getAliPayConfig(rows []*DbSettingRow) *configStruct.AliPayConfig {
 	return &configStruct.AliPayConfig{
-		AppID:      getRow(rows, "ali", "pay", "app_id", "").Value,
-		PrivateKey: getRow(rows, "ali", "pay", "private_key", "").Value,
-		NotifyURL:  getRow(rows, "ali", "pay", "notify_url", "").Value,
-		IsProd:     getRow(rows, "ali", "pay", "is_prod", "false").Value == "1", // 0=调试，1=生产
+		AppID:      GetValueFromRow(rows, "ali", "pay", "app_id", "").Value,
+		PrivateKey: GetValueFromRow(rows, "ali", "pay", "private_key", "").Value,
+		NotifyURL:  GetValueFromRow(rows, "ali", "pay", "notify_url", "").Value,
+		IsProd:     GetValueFromRow(rows, "ali", "pay", "is_prod", "false").Value == "1", // 0=调试，1=生产
 	}
 }
 
 func getRedisConfig(rows []*DbSettingRow) *configStruct.RedisConfig {
 	return &configStruct.RedisConfig{
-		Host:        getRow(rows, "redis", "host", "", "127.0.0.1").Value,
-		Port:        getRow(rows, "redis", "port", "", "6379").Value,
-		Password:    getRow(rows, "redis", "password", "", "").Value,
-		IdleTimeout: typeHelper.Str2Int(getRow(rows, "redis", "idle_timeout", "", "60").Value),
-		Database:    typeHelper.Str2Int(getRow(rows, "redis", "database", "", "").Value),
-		MaxActive:   typeHelper.Str2Int(getRow(rows, "redis", "max_active", "", "10").Value),
-		MaxIdle:     typeHelper.Str2Int(getRow(rows, "redis", "max_idle", "", "10").Value),
+		Host:        GetValueFromRow(rows, "redis", "host", "", "127.0.0.1").Value,
+		Port:        GetValueFromRow(rows, "redis", "port", "", "6379").Value,
+		Password:    GetValueFromRow(rows, "redis", "password", "", "").Value,
+		IdleTimeout: typeHelper.Str2Int(GetValueFromRow(rows, "redis", "idle_timeout", "", "60").Value),
+		Database:    typeHelper.Str2Int(GetValueFromRow(rows, "redis", "database", "", "").Value),
+		MaxActive:   typeHelper.Str2Int(GetValueFromRow(rows, "redis", "max_active", "", "10").Value),
+		MaxIdle:     typeHelper.Str2Int(GetValueFromRow(rows, "redis", "max_idle", "", "10").Value),
 	}
 }
 
 func getEsConfig(rows []*DbSettingRow) *configStruct.EsConfig {
 	return &configStruct.EsConfig{
-		Host:     getRow(rows, "es", "host", "", "http://127.0.0.1").Value,
-		Port:     getRow(rows, "es", "port", "", "9200").Value,
-		Password: getRow(rows, "es", "password", "", "123456").Value,
-		Username: getRow(rows, "es", "username", "", "es").Value,
+		Host:     GetValueFromRow(rows, "es", "host", "", "http://127.0.0.1").Value,
+		Port:     GetValueFromRow(rows, "es", "port", "", "9200").Value,
+		Password: GetValueFromRow(rows, "es", "password", "", "123456").Value,
+		Username: GetValueFromRow(rows, "es", "username", "", "es").Value,
 	}
 }
 
 func getOssConfig(rows []*DbSettingRow) *configStruct.OssConfig {
 	return &configStruct.OssConfig{
-		AccessKeyID:     getRow(rows, "ali", "oss", "access_key_id", "").Value,
-		AccessKeySecret: getRow(rows, "ali", "oss", "access_key_secret", "").Value,
-		Host:            getRow(rows, "ali", "oss", "host", "").Value,
-		EndPoint:        getRow(rows, "ali", "oss", "end_point", "").Value,
-		BucketName:      getRow(rows, "ali", "oss", "bucket_name", "").Value,
-		ExpireTime:      typeHelper.Str2Int64(getRow(rows, "ali", "oss", "expire_time", "30").Value),
+		AccessKeyID:     GetValueFromRow(rows, "ali", "oss", "access_key_id", "").Value,
+		AccessKeySecret: GetValueFromRow(rows, "ali", "oss", "access_key_secret", "").Value,
+		Host:            GetValueFromRow(rows, "ali", "oss", "host", "").Value,
+		EndPoint:        GetValueFromRow(rows, "ali", "oss", "end_point", "").Value,
+		BucketName:      GetValueFromRow(rows, "ali", "oss", "bucket_name", "").Value,
+		ExpireTime:      typeHelper.Str2Int64(GetValueFromRow(rows, "ali", "oss", "expire_time", "30").Value),
 	}
 }
 
 // getAppProfile 项目基础信息
 func getAppProfile(rows []*DbSettingRow) *configStruct.AppProfile {
 	return &configStruct.AppProfile{
-		No:      getRow(rows, "app", "no", "", "").Value,
-		Name:    getRow(rows, "app", "name", "", "").Value,
-		Host:    getRow(rows, "app", "host", "", "").Value,
-		Debug:   getRow(rows, "app", "debug", "", "").Value == "0", // 0=生产，1=调试
-		Version: getRow(rows, "app", "version", "", "").Value,
+		No:      GetValueFromRow(rows, "app", "no", "", "").Value,
+		Name:    GetValueFromRow(rows, "app", "name", "", "").Value,
+		Host:    GetValueFromRow(rows, "app", "host", "", "").Value,
+		Debug:   GetValueFromRow(rows, "app", "debug", "", "").Value == "0", // 0=生产，1=调试
+		Version: GetValueFromRow(rows, "app", "version", "", "").Value,
 	}
 }
 
 // getAliApiConfig 阿里云云市场API服务
 func getAliApiConfig(rows []*DbSettingRow) *configStruct.AliApiConfig {
 	return &configStruct.AliApiConfig{
-		AppID:     getRow(rows, "ali", "api", "app_id", "").Value,
-		AppSecret: getRow(rows, "ali", "api", "app_secret", "").Value,
-		AppCode:   getRow(rows, "ali", "api", "app_code", "").Value,
+		AppID:     GetValueFromRow(rows, "ali", "api", "app_id", "").Value,
+		AppSecret: GetValueFromRow(rows, "ali", "api", "app_secret", "").Value,
+		AppCode:   GetValueFromRow(rows, "ali", "api", "app_code", "").Value,
 	}
 }
 
 // getAliSmsConfig 阿里云短信服务
 func getAliSmsConfig(rows []*DbSettingRow) *configStruct.AliSmsConfig {
 	return &configStruct.AliSmsConfig{
-		AccessKeySecret: getRow(rows, "ali", "sms", "access_key_secret", "").Value,
-		AccessKeyID:     getRow(rows, "ali", "sms", "access_key_id", "").Value,
+		AccessKeySecret: GetValueFromRow(rows, "ali", "sms", "access_key_secret", "").Value,
+		AccessKeyID:     GetValueFromRow(rows, "ali", "sms", "access_key_id", "").Value,
 	}
 }
 
-// getRow : 从rows中提取row
-func getRow(rows []*DbSettingRow, name, flag1, flag2 string, defaultValue string) (row *DbSettingRow) {
+// GetValueFromRow : 从rows中提取row
+func GetValueFromRow(rows []*DbSettingRow, name, flag1, flag2 string, defaultValue string) (row *DbSettingRow) {
 
 	row = &DbSettingRow{}
 
