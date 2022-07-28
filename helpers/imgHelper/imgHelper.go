ba13d5b92d57a75757890a865ea4c7942a5cab16
wiidz <hujiayilu@outlook.com>
2022-07-28 16:40:34 +0800

    fix

--- helpers/imgHelper/imgHelper.go
+++ helpers/imgHelper/imgHelper.go
@@ -50,7 +50,7 @@ func Buff2Image(bytes []byte, filePath string) (err error) {
 }
 
 // MergeLocalImg 拼合本地图片
-func MergeLocalImg(bgImgFilePath string, newFilePath string, coverImgSlice []CoverImgInterface) (err error) {
+func MergeLocalImg(bgImgFilePath string, newFilePath string, coverImgSlice ...CoverImgInterface) (err error) {
 
 	//【1】打开背景图
 	var bgImg image.Image
@@ -121,7 +121,7 @@ func MergeNetworkImg(bgImgURL string, newFilePath string, coverImgSlice []*Netwo
 		localFilePaths = append(localFilePaths, networkURL)
 	}
 
-	err = MergeLocalImg(bgImgFilePath, newFilePath, interfaceSlice)
+	err = MergeLocalImg(bgImgFilePath, newFilePath, interfaceSlice...)
 
 	go func() {
 		for _, v := range localFilePaths {
