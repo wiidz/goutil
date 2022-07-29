18bf6c21c043197aca2b298ebc2019e3e4f883ea
wiidz <hujiayilu@outlook.com>
2022-07-29 14:16:28 +0800

    ifx

--- helpers/osHelper/osHelper.go
+++ helpers/osHelper/osHelper.go
@@ -50,89 +50,6 @@ func ExistSameSizeFile(filename string, filesize int64) bool {
 	return false
 }
 
-// DownloadFile 从url中下载文件
-func DownloadFile(url string, localPath string, fb func(length, downLen int64)) error {
-	var (
-		fsize   int64
-		buf     = make([]byte, 32*1024)
-		written int64
-	)
-	tmpFilePath := localPath + ".download"
-	fmt.Println(tmpFilePath)
-	//创建一个http client
-	client := new(http.Client)
-	//client.Timeout = time.Second * 60 //设置超时时间
-	//get方法获取资源
-	resp, err := client.Get(url)
-	if err != nil {
-		return err
-	}
-
-	//读取服务器返回的文件大小
-	contentLen := resp.Header.Get("Content-Length")
-	// 如果能获取文件长度 则判断是否存在
-	if contentLen != "" {
-		fsize, err = strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 32)
-		if err != nil {
-			fmt.Println(err)
-		}
-		if ExistSameSizeFile(localPath, fsize) {
-			return err
-		}
-		fmt.Println("fsize", fsize)
-	}
-
-	//创建文件
-	file, err := os.Create(tmpFilePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	if resp.Body == nil {
-		return errors.New("body is null")
-	}
-	defer resp.Body.Close()
-	//下面是 io.copyBuffer() 的简化版本
-	for {
-		//读取bytes
-		nr, er := resp.Body.Read(buf)
-		if nr > 0 {
-			//写入bytes
-			nw, ew := file.Write(buf[0:nr])
-			//数据长度大于0
-			if nw > 0 {
-				written += int64(nw)
-			}
-			//写入出错
-			if ew != nil {
-				err = ew
-				break
-			}
-			//读取是数据长度不等于写入的数据长度
-			if nr != nw {
-				err = io.ErrShortWrite
-				break
-			}
-		}
-		if er != nil {
-			if er != io.EOF {
-				err = er
-			}
-			break
-		}
-		//没有错误了快使用 callback
-		if fb != nil {
-			fb(fsize, written)
-		}
-	}
-	if err == nil {
-		file.Close()
-		err = os.Rename(tmpFilePath, localPath)
-		fmt.Println(err)
-	}
-	return err
-}
-
 // ReadJsonFile 读取json格式的文件
 func ReadJsonFile(filePath string) map[string]interface{} {
 	file, _ := os.Open(filePath)
@@ -216,18 +133,105 @@ func Delete(filePath string) error {
 	return os.Remove(filePath)
 }
 
+// DownloadFile 从url中下载文件
+func DownloadFile(url string, localPath string, fb func(length, downLen int64)) error {
+	var (
+		fsize   int64
+		buf     = make([]byte, 32*1024)
+		written int64
+	)
+	tmpFilePath := localPath + ".download"
+	fmt.Println(tmpFilePath)
+	//创建一个http client
+	client := new(http.Client)
+	//client.Timeout = time.Second * 60 //设置超时时间
+	//get方法获取资源
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+
+	//读取服务器返回的文件大小
+	contentLen := resp.Header.Get("Content-Length")
+	// 如果能获取文件长度 则判断是否存在
+	if contentLen != "" {
+		fsize, err = strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 32)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if ExistSameSizeFile(localPath, fsize) {
+			return err
+		}
+		fmt.Println("fsize", fsize)
+	}
+
+	//创建文件
+	file, err := os.Create(tmpFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if resp.Body == nil {
+		return errors.New("body is null")
+	}
+	defer resp.Body.Close()
+	//下面是 io.copyBuffer() 的简化版本
+	for {
+		//读取bytes
+		nr, er := resp.Body.Read(buf)
+		if nr > 0 {
+			//写入bytes
+			nw, ew := file.Write(buf[0:nr])
+			//数据长度大于0
+			if nw > 0 {
+				written += int64(nw)
+			}
+			//写入出错
+			if ew != nil {
+				err = ew
+				break
+			}
+			//读取是数据长度不等于写入的数据长度
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+		//没有错误了快使用 callback
+		if fb != nil {
+			fb(fsize, written)
+		}
+	}
+	if err == nil {
+		file.Close()
+		err = os.Rename(tmpFilePath, localPath)
+		fmt.Println(err)
+	}
+	return err
+}
+
 // SimpleDownloadFile 简单下载（下载到当前工程目录的/tmp下，记得给权限）
-func SimpleDownloadFile(url string) (localPath string, err error) {
+func SimpleDownloadFile(url string) (finalFilePath string, err error) {
 	var (
 		fsize   int64
 		buf     = make([]byte, 32*1024)
 		written int64
 	)
 
-	_, _, fileType := networkHelper.GetFileNameFromURL(url)
-	tempName := strHelper.GetRandomString(8) + "." + fileType
 	dir, _ := os.Getwd()
-	localPath = dir + "/temp/" + tempName
+
+	_, _, fileType := networkHelper.GetFileNameFromURL(url)
+
+	tempName := strHelper.GetRandomString(8)
+
+	tempPath := dir + "/temp/" + tempName + ".download"  // 临时路径
+	finalFilePath = dir + "/temp/" + tempName + fileType // 最终路径
 
 	//创建一个http client
 	client := new(http.Client)
@@ -247,14 +251,14 @@ func SimpleDownloadFile(url string) (localPath string, err error) {
 		if err != nil {
 			fmt.Println(err)
 		}
-		if ExistSameSizeFile(localPath, fsize) {
+		if ExistSameSizeFile(tempPath, fsize) {
 			return
 		}
 		fmt.Println("fsize", fsize)
 	}
 
 	// 创建文件
-	file, err := os.Create(localPath)
+	file, err := os.Create(tempPath)
 	if err != nil {
 		return
 	}
@@ -294,7 +298,11 @@ func SimpleDownloadFile(url string) (localPath string, err error) {
 			break
 		}
 	}
-
+	if err == nil {
+		file.Close()
+		err = os.Rename(tempPath, finalFilePath)
+		fmt.Println(err)
+	}
 	return
 }
 
