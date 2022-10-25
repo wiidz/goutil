586e8fc6f1abc69bee7adf02cfbeb3c0b8bed0e9
wiidz <hujiayilu@outlook.com>
2022-10-25 14:51:02 +0800

    add DataStepping

---
+++ mngs/mysqlMng/dataChange.go
@@ -0,0 +1,34 @@
+package mysqlMng
+
+import (
+	"github.com/wiidz/goutil/helpers/typeHelper"
+	"gorm.io/gorm"
+)
+
+type SteppingWay int8 // 步进方式
+
+const Increase SteppingWay = 1 // 增
+const Decrease SteppingWay = 2 // 减
+
+type DataSteppingParam struct {
+	ID        uint64
+	Way       SteppingWay
+	Model     interface{}
+	FieldName string
+	Amount    int64
+}
+
+func (p *DataSteppingParam) GetOperator() (operator string) {
+
+	if p.Way == Increase {
+		operator = " - "
+	} else {
+		operator = " + "
+	}
+	return
+}
+
+// DataStepping 针对一个数字字段递增或递减
+func DataStepping(conn *gorm.DB, params *DataSteppingParam) (err error) {
+	return conn.Model(&params.Model).Where("id = ?", &params.ID).Update(params.FieldName, gorm.Expr(params.FieldName+params.GetOperator()+typeHelper.Int64ToStr(params.Amount))).Error
+}
