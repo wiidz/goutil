368bd6b919c15c91a91c4694041ab8f6cd5ffe2c
胡加一路 <>
2022-12-07 15:05:20 +0800

    fix

--- mngs/lbsMng/structs.go
+++ mngs/lbsMng/structs.go
@@ -17,18 +17,18 @@ type ReGeoData struct {
 }
 
 type AddressComponent struct {
-	Country       string       `json:"country"`  // 国家：中国
-	Province      string       `json:"province"` // 省份：北京市
-	City          string       `json:"city"`     // 城市：
-	CityCode      string       `json:"citycode"` // 城市编码：010
-	District      string       `json:"district"` // 街道：朝阳区
-	AdCode        string       `json:"adcode"`   // 邮政编码：110105
-	Township      string       `json:"township"` // 乡镇：望京街道
-	TownCode      string       `json:"towncode"` // 乡镇编号：110105026000
-	Neighborhood  *Neighborhood   `json:"neighborhood"`
-	Building      *Neighborhood   `json:"building"`
-	StreetNumber  *Street      `json:"streetNumber"`
-	BusinessAreas []*MetaPlace `json:"businessAreas"`
+	Country       string        `json:"country"`  // 国家：中国
+	Province      string        `json:"province"` // 省份：北京市
+	City          string        `json:"city"`     // 城市：
+	CityCode      string        `json:"citycode"` // 城市编码：010
+	District      string        `json:"district"` // 街道：朝阳区
+	AdCode        string        `json:"adcode"`   // 邮政编码：110105
+	Township      string        `json:"township"` // 乡镇：望京街道
+	TownCode      string        `json:"towncode"` // 乡镇编号：110105026000
+	Neighborhood  *Neighborhood `json:"neighborhood"`
+	Building      *Neighborhood `json:"building"`
+	StreetNumber  *Street       `json:"streetNumber"`
+	BusinessAreas []*MetaPlace  `json:"businessAreas"`
 }
 
 type MetaPlace struct {
@@ -39,11 +39,10 @@ type MetaPlace struct {
 
 type Neighborhood struct {
 	//ID       string `json:"id"`
-	Name     []string `json:"name"`     // 名称：方恒国际中心
-	Type     []string `json:"type"`     // 类型：商务住宅;楼宇;商务写字楼
+	Name []string `json:"name"` // 名称：方恒国际中心
+	Type []string `json:"type"` // 类型：商务住宅;楼宇;商务写字楼
 	//Location string `json:"location"` // 经纬度：（经度，纬度）
 }
-
 
 type Street struct {
 	Street    string `json:"street"`    // 街道名：阜通东大街
@@ -55,37 +54,62 @@ type Street struct {
 
 // RouteRes 驾车路线
 type RouteRes struct {
-	Count string `json:"count"`
-	Route *Route `json:"route"`
+	Count      string           `json:"count"`
+	Route      *Route           `json:"route"`
+	ShowFields *RouteShowFields `json:"show_fields"` // 可选差异化结果返回
 	*BaseRes
+}
+type RouteShowFields struct {
+	Cost *Cost `json:"cost"` // 设置后可返回方案所需时间及费用成本
+	Tmcs *Tmcs `json:"tmcs"` // 设置后可返回分段路况详情
+	//Navi     `json:"navi"`     // 设置后可返回详细导航动作指令
+	//Cities   `json:"cities"`   // 设置后可返回分段途径城市信息
+	//polyline `json:"Polyline"` // 设置后可返回分路段坐标点串，两点间用“,”分隔
+}
+
+type Cost struct {
+	Duration      string `json:"duration"`       // 线路耗时，分段step中的耗时
+	Tolls         string `json:"tolls"`          // 线路耗时，分段step中的耗时
+	TollDistance  string `json:"toll_distance"`  // 线路耗时，分段step中的耗时
+	TollRoad      string `json:"toll_road"`      // 线路耗时，分段step中的耗时
+	TrafficLights string `json:"traffic_lights"` // 线路耗时，分段step中的耗时
+}
+
+type Tmcs struct {
+	TmcStatus    string `json:"tmc_status"`   // 路况信息，包括：未知、畅通、缓行、拥堵、严重拥堵
+	Tolls        string `json:"tmc_distance"` // 从当前坐标点开始step中路况相同的距离
+	TollDistance string `json:"tmc_polyline"` // 此段路况涉及的道路坐标点串，点间用","分隔
 }
 
 type Route struct {
-	Origin      string  `json:"origin"`
-	Destination string  `json:"destination"`
-	Paths       []*Path `json:"paths"`
+	Origin      string  `json:"origin"`      // 起点经纬度
+	Destination string  `json:"destination"` // 终点经纬度
+	TaxiCost    string  `json:"taxi_cost"`   // 预计出租车费用，单位：元
+	Paths       []*Path `json:"paths"`       // 算路方案详情
 }
 
 type Path struct {
-	Distance      string      `json:"distance"`       // 27876
-	Duration      string      `json:"duration"`       // 4197
-	Strategy      string      `json:"strategy"`       // 速度最快
-	Tolls         interface{} `json:"tolls"`          // 0
-	TollDistance  interface{} `json:"toll_distance"`  // 0
-	Restriction   string      `json:"restriction"`    // 0
-	TrafficLights string      `json:"traffic_lights"` // 23
-	Steps         []*Step     `json:"steps"`
+	Distance string `json:"distance"` // 方案距离，单位：米 27876
+	//Duration      string      `json:"duration"`       // 4197
+	//Strategy      string      `json:"strategy"`       // 速度最快
+	//Tolls         interface{} `json:"tolls"`          // 0
+	//TollDistance  interface{} `json:"toll_distance"`  // 0
+	Restriction string `json:"restriction"` // 0 代表限行已规避或未限行，即该路线没有限行路段  1 代表限行无法规避，即该线路有限行路段
+	//TrafficLights string  `json:"traffic_lights"` // 23
+	Steps []*Step `json:"steps"` // 路线分段
 }
 
 type Step struct {
-	Instruction     string      `json:"instruction"`      // 向西南行驶44米右转进入主路
-	Orientation     string      `json:"orientation"`      // 西南
-	Distance        string      `json:"distance"`         // 44
-	Tolls           interface{} `json:"tolls"`            // 0
-	TollDistance    interface{} `json:"toll_distance"`    // 0
-	TollRoad        interface{} `json:"toll_road"`        // []
-	Duration        string      `json:"duration"`         // 5
-	Polyline        string      `json:"polyline"`         // 116.481216,39.989532;116.48101,39.989311;116.480957,39.989262;116.480904,39.989216
-	Action          interface{} `json:"action"`           // 右转 或者 []
-	AssistantAction interface{} `json:"assistant_action"` // 进入主路 或者 []
+	Instruction  string `json:"instruction"`   // 行驶指示 向西南行驶44米右转进入主路
+	Orientation  string `json:"orientation"`   // 进入道路方向 西南
+	RoadName     string `json:"road_name"`     // 分段道路名称
+	StepDistance string `json:"step_distance"` // 分段距离信息 44
+
+	//Tolls           interface{} `json:"tolls"`            // 0
+	//TollDistance    interface{} `json:"toll_distance"`    // 0
+	//TollRoad        interface{} `json:"toll_road"`        // []
+	//Duration        string      `json:"duration"`         // 5
+	//Polyline        string      `json:"polyline"`         // 116.481216,39.989532;116.48101,39.989311;116.480957,39.989262;116.480904,39.989216
+	//Action          interface{} `json:"action"`           // 右转 或者 []
+	//AssistantAction interface{} `json:"assistant_action"` // 进入主路 或者 []
 }
