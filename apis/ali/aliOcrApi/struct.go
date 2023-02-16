ba9b112a8709553e31094e59eea80f7c25d81f1d
胡加一路 <>
2023-02-17 00:05:42 +0800

    add ocr err hanler

--- apis/ali/aliOcrApi/struct.go
+++ apis/ali/aliOcrApi/struct.go
@@ -7,8 +7,8 @@ const Back CardSide = "back"  // 反面（国徽面等）
 
 type OcrError struct {
 	OriginalErr error  `json:"original_err"` // 原始错误信息
-	Code        string `json:"Cope"`
-	HostId      string `json:"HostId"`
+	Code        string `json:"Code"`
+	HostID      string `json:"HostId"`
 	Message     string `json:"Message"`
 	Recommend   string `json:"Recommend"`
 	RequestId   string `json:"RequestId"`
