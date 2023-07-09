5aaac2f5b106fccd02b384fc12f0f5bb985c2c45
胡加一路 <hujiayilu@outlook.com>
2023-07-09 21:31:58 +0800

    add bemfamng

---
+++ mngs/bemfaMng/bemfaMng.go
@@ -0,0 +1,161 @@
+package bemfaMng
+
+import (
+	"errors"
+	"github.com/wiidz/goutil/helpers/networkHelper"
+	"github.com/wiidz/goutil/helpers/typeHelper"
+	"github.com/wiidz/goutil/structs/networkStruct"
+)
+
+const Domain = "https://apis.bemfa.com"
+
+// NewBemfaMng  获取es管理器
+func NewBemfaMng(UID, topicID string) (es *BemfaMng) {
+	return &BemfaMng{
+		UID:     UID,
+		TopicID: topicID,
+	}
+}
+
+// SwitchOn 打开1路开关
+func (mng *BemfaMng) SwitchOn(weMsg string) (data *ReturnBase, err error) {
+	var url = Domain + "/va/postJsonMsg"
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkStruct.Post, url, map[string]interface{}{
+		"uid":   mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic": mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":  3,           // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+		"msg":   "a1",        // 必填，消息体，要推送的消息，自定义即可，比如on，或off等等
+		"wemsg": weMsg,       // 选填，发送到微信的消息，自定义即可。如果携带此字段，会将消息发送到微信
+	}, map[string]string{}, &ReturnBase{})
+	return res.(*ReturnBase), err
+}
+
+// SwitchOff 关闭1路开关
+func (mng *BemfaMng) SwitchOff(weMsg string) (data *ReturnBase, err error) {
+	var url = Domain + "/va/postJsonMsg"
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkStruct.Post, url, map[string]interface{}{
+		"uid":   mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic": mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":  3,           // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+		"msg":   "b1",        // 必填，消息体，要推送的消息，自定义即可，比如on，或off等等
+		"wemsg": weMsg,       // 选填，发送到微信的消息，自定义即可。如果携带此字段，会将消息发送到微信
+	}, map[string]string{}, &ReturnBase{})
+	return res.(*ReturnBase), err
+}
+
+// SendMsg 推送消息
+// 向主题推送消息，支持POST协议
+func (mng *BemfaMng) SendMsg(msg, weMsg string) (data *ReturnBase, err error) {
+	var url = Domain + "/va/postJsonMsg"
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkStruct.Post, url, map[string]interface{}{
+		"uid":   mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic": mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":  3,           // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+		"msg":   msg,         // 必填，消息体，要推送的消息，自定义即可，比如on，或off等等
+		"wemsg": weMsg,       // 选填，发送到微信的消息，自定义即可。如果携带此字段，会将消息发送到微信
+	}, map[string]string{}, &ReturnBase{})
+	return res.(*ReturnBase), err
+}
+
+// GetMsg 获取消息
+// 获取主题消息，支持GET协议
+func (mng *BemfaMng) GetMsg(msgAmount int) (data *GetMsgResult, err error) {
+	var url = Domain + "/va/getmsg"
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkStruct.Get, url, map[string]interface{}{
+		"uid":   mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic": mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":  3,           // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+		"num":   msgAmount,   // 选填，获取的历史数据条数，不填默认默认是1，最大5000
+	}, map[string]string{}, &GetMsgResult{})
+	return res.(*GetMsgResult), err
+}
+
+// GetAllTopic 获取全部主题
+func (mng *BemfaMng) GetAllTopic() (data *AllTopicResult, err error) {
+	var url = Domain + "/va/alltopic"
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkStruct.Get, url, map[string]interface{}{
+		"uid":  mng.UID, // 必填，用户私钥，巴法云控制台获取
+		"type": 3,       // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+	}, map[string]string{}, &GetMsgResult{})
+	return res.(*AllTopicResult), err
+}
+
+// IsOnline 判断设备是否在线
+// 获取主题消息，支持GET协议
+func (mng *BemfaMng) IsOnline() (isOnline bool, err error) {
+	var url = Domain + "/va/online"
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkStruct.Get, url, map[string]interface{}{
+		"uid":   mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic": mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":  3,           // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+	}, map[string]string{}, &IsOnlineResult{})
+
+	if err != nil {
+		return
+	}
+
+	isOnline = res.(*IsOnlineResult).Data
+	return
+}
+
+// SetTimer 设置定时操作
+func (mng *BemfaMng) SetTimer(msg string, hour, min, second int) (ok bool, err error) {
+	var url = Domain + "/cloud/settime/v1/"
+	var timeStr = typeHelper.Int2Str(hour) + ":" + typeHelper.Int2Str(min) + ":" + typeHelper.Int2Str(second)
+	res, _, _, err := networkHelper.RequestWithStruct(networkStruct.Post, networkStruct.BodyForm, url, map[string]interface{}{
+		"uid":    mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic":  mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":   3,           // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+		"msg":    msg,         // 必填，消息体，即定时发送的消息,比如等于on或者off
+		"time":   timeStr,     // 必填，时间，格式为 小时:分钟:秒，中间":"为英文格式符号，小时0-23，分钟0-59，秒0-59
+		"action": "add",       // 必填，动作，action=add时是添加定时，action=del是删除定时
+	}, map[string]string{}, &ReturnStatus{})
+
+	if err != nil {
+		return
+	}
+
+	data := res.(*ReturnStatus)
+	if data.Code == int(AddSuccess) {
+		return true, nil
+	}
+
+	return false, errors.New(data.Status)
+
+}
+
+// DeleteTimer 删除定时操作
+func (mng *BemfaMng) DeleteTimer() (ok bool, err error) {
+	var url = Domain + "/cloud/settime/v1/"
+	res, _, _, err := networkHelper.RequestWithStruct(networkStruct.Delete, networkStruct.BodyForm, url, map[string]interface{}{
+		"uid":   mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic": mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":  3,           // 必填，主题类型，当type=1时是MQTT协议，3是TCP协议
+		//"msg":    msg,         // 必填，消息体，即定时发送的消息,比如等于on或者off
+		//"time":   timeStr,     // 必填，时间，格式为 小时:分钟:秒，中间":"为英文格式符号，小时0-23，分钟0-59，秒0-59
+		"action": "del", // 必填，动作，action=add时是添加定时，action=del是删除定时
+	}, map[string]string{}, &ReturnStatus{})
+
+	if err != nil {
+		return
+	}
+
+	data := res.(*ReturnStatus)
+	if data.Code == int(DeleteSuccess) {
+		return true, nil
+	}
+
+	return false, errors.New(data.Status)
+
+}
+
+// GetTimer 获取定时操作
+func (mng *BemfaMng) GetTimer() (data *ReturnBase, err error) {
+	var url = Domain + "/cloud/settime/v1/"
+	res, _, _, err := networkHelper.RequestJsonWithStruct(networkStruct.Get, url, map[string]interface{}{
+		"uid":   mng.UID,     // 必填，用户私钥，巴法云控制台获取
+		"topic": mng.TopicID, // 必填，主题名，可在控制台创建
+		"type":  3,           // 必填，设备类型，当type=3时为创客云设备，当等于2时为设备云设备，当等于1时为MQTT设备
+	}, map[string]string{}, &ReturnBase{})
+	return res.(*ReturnBase), err
+}
---
+++ mngs/bemfaMng/struct.go
@@ -0,0 +1,90 @@
+package bemfaMng
+
+// BemfaMng 巴法云
+type BemfaMng struct {
+	UID     string `json:"uid"`
+	TopicID string `json:"topic_id"`
+}
+
+type ReturnBase struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type ReturnStatus struct {
+	Code   int    `json:"code"`
+	Status string `json:"status"`
+}
+
+type GetMsgResult struct {
+	Code    int           `json:"code"`
+	Message string        `json:"message"`
+	Data    []HistoryData `json:"data"`
+}
+
+type HistoryData struct {
+	Msg  string `json:"msg"`  // 获取的主题消息
+	Time string `json:"time"` // 消息发送的时间，时区UTC/GMT+08:00
+	Unix int    `json:"unix"` // 消息发送的时间戳
+}
+
+// AllTopicResult 获取全部主题
+type AllTopicResult struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    []TopicData `json:"data"`
+}
+
+type TopicData struct {
+	Topic  string `json:"topic"`  // 主题值
+	Msg    string `json:"msg"`    // 消息体
+	Name   string `json:"name"`   // 主题名字
+	Online bool   `json:"online"` // 是否在线
+	Tid    string `json:"tid"`    // 设备类型
+	Sid    string `json:"sid"`    // 如果是分享设备，此字段是分享者密钥
+	Time   string `json:"time"`   // 消息发送的时间，时区UTC/GMT+08:00
+	Unix   int    `json:"unix"`   // 消息发送的时间戳
+}
+
+type TID string
+
+type IsOnlineResult struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    bool   `json:"data"`
+}
+
+const Outlet TID = "outlet"             // 插座
+const Light TID = "light"               // 灯
+const Fan TID = "fan"                   // 风扇
+const Sensor TID = "sensor"             // 传感器
+const AirCondition TID = "aircondition" // 空调
+const Switch TID = "switch"             // 开关
+const Curtain TID = "curtain"           // 窗帘
+
+type ReturnCode int
+
+const Success ReturnCode = 0 // 成功
+
+const ParamError ReturnCode = 10002 // 请求参数有误
+
+const UnknownError ReturnCode = 40000 // 未知错误
+const KeyError ReturnCode = 40004     // 私钥或主题错误
+
+// 定时相关
+const NoUIDError ReturnCode = 4003001       // 缺少uid字段
+const WrongUIDError ReturnCode = 4003002    // uid值为空或不正确
+const NoTopicError ReturnCode = 4003003     // 缺少topic字段
+const WrongTopicError ReturnCode = 4003004  // topic值为空或不正确
+const NoTypeError ReturnCode = 4003005      // 缺少type字段
+const WrongTypeError ReturnCode = 4003006   // type值为空或不正确
+const NoTimeError ReturnCode = 4003007      // 缺少time字段
+const WrongTimeError ReturnCode = 4003008   // time值为空或不正确
+const NoMsgError ReturnCode = 4003009       // 缺少msg字段
+const WrongMsgError ReturnCode = 4003010    // msg值为空或不正确
+const NoActionError ReturnCode = 4003011    // 缺少action字段
+const WrongActionError ReturnCode = 4003012 // action值为空或不正确
+const AddSuccess ReturnCode = 4003013       // 添加成功
+const DeleteSuccess ReturnCode = 4003014    // 删除
+const Existed ReturnCode = 4003015          // 定时已存在
