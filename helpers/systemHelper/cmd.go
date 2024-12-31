bcbe26f029728ac4a14f444491941333e2348b61
hujiayilu <hujiayilu@outlook.com>
2025-01-01 01:29:47 +0800

    fix

--- helpers/systemHelper/cmd.go
+++ helpers/systemHelper/cmd.go
@@ -4,10 +4,10 @@ import (
 	"os/exec"
 )
 
-func ExecCmd(cmdLine string) (res string, err error) {
+func ExecCmd(name string, arg ...string) (res string, err error) {
 
 	// 定义要执行的命令
-	cmd := exec.Command(cmdLine)
+	cmd := exec.Command(name, arg...)
 
 	// 执行命令并捕获输出和错误
 	output, err := cmd.CombinedOutput()
