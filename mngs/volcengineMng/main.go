5a293d1349767173b38f0203da79f541e247487c
hujiayilu <hujiayilu@outlook.com>
2025-11-06 16:39:05 +0800

    fix aim

--- mngs/volcengineMng/main.go
+++ mngs/volcengineMng/main.go
@@ -1,8 +1,11 @@
 package volcengineMng
 
 import (
+	"context"
 	"errors"
 	"github.com/volcengine/volcengine-go-sdk/service/arkruntime"
+	"github.com/volcengine/volcengine-go-sdk/service/arkruntime/model"
+	"github.com/volcengine/volcengine-go-sdk/volcengine"
 	"github.com/wiidz/goutil/structs/configStruct"
 )
 
@@ -23,5 +26,34 @@ func NewVolcengineMng(config *configStruct.VolcengineConfig) (mng *VolcengineMng
 
 func (mng *VolcengineMng) initClient() {
 	mng.Client = arkruntime.NewClientWithApiKey(mng.Config.ApiKey)
+	return
+}
+
+// CreateChatCompletionRequest 创建文本对话
+func (mng *VolcengineMng) CreateChatCompletionRequest(ctx context.Context, aiModel AIModel, thinking ThinkingType, params []*ChatParam) (response model.ChatCompletionResponse, err error) {
+
+	//【1】处理对话文本
+	var msgs = []*model.ChatCompletionMessage{}
+	for _, v := range params {
+		temp := &model.ChatCompletionMessage{
+			Role: string(v.Role),
+			Content: &model.ChatCompletionMessageContent{
+				StringValue: volcengine.String(v.Text),
+			},
+		}
+		msgs = append(msgs, temp)
+	}
+
+	//【2】组合参数
+	req := model.CreateChatCompletionRequest{
+		Model:    string(aiModel),
+		Messages: msgs,
+		Thinking: &model.Thinking{
+			Type: thinking.GetThinkingType(),
+		},
+	}
+
+	//【3】发送请求
+	response, err = mng.Client.CreateChatCompletion(ctx, req)
 	return
 }
