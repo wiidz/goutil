0db658e203bba41f11d1836a852d4245d44c028d
hujiayilu <hujiayilu@outlook.com>
2025-11-08 17:17:03 +0800

    fix

--- mngs/audioToTextMng/main.go
+++ mngs/audioToTextMng/main.go
@@ -31,3 +31,21 @@ func (mng *AudioToTextMng) Generate(voice, voiceUrl, format string) (data *Resp,
 
 	return resStr.(*Resp), nil
 }
+
+// Generate 语音转文字
+func (mng *AudioToTextMng) GenerateRaw(voice, voiceUrl, format string) (data string, err error) {
+
+	resStr, _, _, err := networkHelper.RequestRaw(networkStruct.Post, URL, map[string]interface{}{
+		"voice":    voice,    // 语音文件，不超过1MB，和voiceUrl二选一
+		"voiceUrl": voiceUrl, // 音频文件url，下载音频不超过1MB，和voice二选一
+		"format":   format,   // 语音文件的格式，pcm/wav/amr/m4a。不区分大小写。推荐pcm文件
+	}, map[string]string{
+		"Authorization": "APPCODE " + mng.Config.AppCode,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return resStr, nil
+}
