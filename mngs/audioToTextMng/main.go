b37c0d43edc6b4aea85aeecd73fc16ba62ce9ee0
hujiayilu <hujiayilu@outlook.com>
2025-11-08 17:48:11 +0800

    fix

--- mngs/audioToTextMng/main.go
+++ mngs/audioToTextMng/main.go
@@ -1,9 +1,8 @@
 package audioToTextMng
 
 import (
-	"log"
+	"encoding/json"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/wiidz/goutil/helpers/networkHelper"
 	"github.com/wiidz/goutil/structs/configStruct"
 	"github.com/wiidz/goutil/structs/networkStruct"
@@ -18,9 +17,9 @@ func New(config *configStruct.AliApiConfig) *AudioToTextMng {
 const URL = "https://smyuyin.market.alicloudapi.com/v2/voice_to_text/generate"
 
 // Generate 语音转文字
-func (mng *AudioToTextMng) Generate(voice, voiceUrl, format string) (data []byte, err error) {
+func (mng *AudioToTextMng) Generate(voice, voiceUrl, format string) (data *Resp, err error) {
 
-	res, header, httpCode, err := networkHelper.SendRequest(networkStruct.Post, networkStruct.XWWWForm, URL, map[string]interface{}{
+	res, _, _, err := networkHelper.SendRequest(networkStruct.Post, networkStruct.XWWWForm, URL, map[string]interface{}{
 		"voice":    voice,    // 语音文件，不超过1MB，和voiceUrl二选一
 		"voiceUrl": voiceUrl, // 音频文件url，下载音频不超过1MB，和voice二选一
 		"format":   format,   // 语音文件的格式，pcm/wav/amr/m4a。不区分大小写。推荐pcm文件
@@ -32,8 +31,9 @@ func (mng *AudioToTextMng) Generate(voice, voiceUrl, format string) (data []byte
 		return nil, err
 	}
 
-	log.Println("httpCode:", httpCode)
-	spew.Dump(header)
+	//log.Println("httpCode:", httpCode)
+	//spew.Dump(header)
 
-	return res, nil
+	err = json.Unmarshal(res, &data)
+	return
 }
