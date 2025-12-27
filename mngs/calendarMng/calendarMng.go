6a92ba67c7571d0bc8030ee024fdac5cb5a73fb7
hujiayilu <hujiayilu@outlook.com>
2025-12-27 15:41:36 +0800

    add calendarMng

---
+++ mngs/calendarMng/calendarMng.go
@@ -0,0 +1,107 @@
+package calendarMng
+
+import (
+	"fmt"
+
+	"github.com/wiidz/goutil/helpers/networkHelper"
+	"github.com/wiidz/goutil/structs/configStruct"
+	"github.com/wiidz/goutil/structs/networkStruct"
+)
+
+const URL = "https://jmhlysjjr.market.alicloudapi.com"
+
+// 以下方法均为聚美智数在阿里云市场中提供的服务
+// https://market.aliyun.com/detail/cmapi00066017?spm=5176.730005.result.2.325e414avf3OMU&innerSource=search_%E6%97%A5%E5%8E%86#sku=yuncode6001700003
+type CalendarMng struct {
+	Config *configStruct.AliApiConfig
+}
+
+// NewCalendarMng : 返回日历管理器
+func NewCalendarMng(config *configStruct.AliApiConfig) *CalendarMng {
+	return &CalendarMng{
+		Config: config,
+	}
+}
+
+// request 统一的请求处理函数
+// 检查错误和响应码，返回 Data 字段
+func request[T any](
+	mng *CalendarMng,
+	method networkStruct.Method,
+	url string,
+	params map[string]interface{},
+	respStruct *BaseResp[T],
+) (*T, error) {
+	resStr, _, _, err := networkHelper.RequestJsonWithStruct(
+		method,
+		url,
+		params,
+		map[string]string{
+			"Authorization": "APPCODE " + mng.Config.AppCode,
+		},
+		respStruct,
+		false,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, ok := resStr.(*BaseResp[T])
+	if !ok {
+		return nil, fmt.Errorf("响应类型转换失败")
+	}
+
+	if resp.Code != 200 {
+		return nil, fmt.Errorf("API返回错误: code=%d, msg=%s", resp.Code, resp.Msg)
+	}
+
+	return resp.Data, nil
+}
+
+// GetHolidayList 获取节假日列表
+// date string 需要查询的日期，格式：YYYYMMDD（选填）
+// needDesc string 是否需要返回当日公众日、国际日和我国传统节日的简介，1-返回，默认不返回（选填）
+func (mng *CalendarMng) GetHolidayList(date string, needDesc string) (data *HolidayData, err error) {
+	params := make(map[string]interface{})
+	if date != "" {
+		params["date"] = date
+	}
+	if needDesc != "" {
+		params["needDesc"] = needDesc
+	}
+
+	return request(mng, networkStruct.Post, URL+"/holiday/list", params, &HolidayResponse{})
+}
+
+// GetHolidayDetail 获取节假日详情
+// year string 需要查询的年份，格式：YYYY
+func (mng *CalendarMng) GetHolidayDetail(year string) (data *HolidayDetailData, err error) {
+	return request(mng, networkStruct.Post, URL+"/holiday/detail", map[string]interface{}{
+		"year": year,
+	}, &HolidayDetailResp{})
+}
+
+// GetAuspiciousTime 获取时辰吉凶数据
+// date string 需要查询的日期，格式：YYYYMMDD
+func (mng *CalendarMng) GetAuspiciousTime(date string) (data *AuspiciousTimeData, err error) {
+	return request(mng, networkStruct.Post, URL+"/luck-tendency/auspicious-time", map[string]interface{}{
+		"date": date,
+	}, &AuspiciousTimeResp{})
+}
+
+// GetAuspiciousDemon 获取宜忌神煞数据
+// date string 需要查询的日期，格式：YYYYMMDD
+func (mng *CalendarMng) GetAuspiciousDemon(date string) (data *AuspiciousDemonData, err error) {
+	return request(mng, networkStruct.Post, URL+"/luck-tendency/auspicious-demon", map[string]interface{}{
+		"date": date,
+	}, &AuspiciousDemonResp{})
+}
+
+// GetAlmanac 获取黄历数据
+// date string 需要查询的日期，格式：YYYYMMDD
+func (mng *CalendarMng) GetAlmanac(date string) (data *AlmanacData, err error) {
+	return request(mng, networkStruct.Post, URL+"/luck-tendency/almanac", map[string]interface{}{
+		"date": date,
+	}, &AlmanacResp{})
+}
---
+++ mngs/calendarMng/struct.go
@@ -0,0 +1,136 @@
+package calendarMng
+
+// BaseResp 通用API响应结构
+type BaseResp[T any] struct {
+	Code   int    `json:"code"`   // 返回码，详见返回码说明
+	Msg    string `json:"msg"`    // 返回消息
+	TaskNo string `json:"taskNo"` // 请求号
+	Data   *T     `json:"data"`   // 响应数据
+}
+
+// HolidayResponse 节假日API响应结构
+type HolidayResponse = BaseResp[HolidayData]
+
+// Data 节假日数据
+type HolidayData struct {
+	Count int           `json:"count"` // 一年的节假日数量
+	Items []HolidayItem `json:"items"` // 一年的节假日列表
+}
+
+// HolidayItem 节假日项
+type HolidayItem struct {
+	Begin         string   `json:"begin"`          // 节假日的开始时间，格式：YYYYMMDD
+	End           string   `json:"end"`            // 节假日的结束时间，格式：YYYYMMDD
+	Holiday       string   `json:"holiday"`        // 节假日名称
+	HolidayRemark string   `json:"holiday_remark"` // 节假日描述
+	InverseDays   []string `json:"inverse_days"`   // 调修日列表，只有2021年之后的数据有值，格式：YYYYMMDD
+}
+
+// AlmanacResp 黄历API响应结构
+type AlmanacResp = BaseResp[AlmanacData]
+
+// AlmanacData 黄历数据
+type AlmanacData struct {
+	Gongli    string `json:"gongli"`    // 公历，格式：公元 YYYY年MM月DD日 星期X
+	Nongli    string `json:"nongli"`    // 农历
+	Jieri     string `json:"jieri"`     // 节日
+	Zhiri     string `json:"zhiri"`     // 值日
+	Zhishen   string `json:"zhishen"`   // 值神
+	Yi        string `json:"yi"`        // 宜
+	Ji        string `json:"ji"`        // 忌
+	Qixiang   string `json:"qixiang"`   // 气象
+	Jieqi24   string `json:"jieqi24"`   // 当前月包含的24节气
+	Shengxiao string `json:"shengxiao"` // 生肖
+	Xingzuo   string `json:"xingzuo"`   // 星座
+	Rulueli   string `json:"rulueli"`   // 儒略历
+	Jsyq      string `json:"jsyq"`      // 吉神宜趋
+	Xsyj      string `json:"xsyj"`      // 凶神宜忌
+	Pzbj      string `json:"pzbj"`      // 彭祖百忌
+	Tszf      string `json:"tszf"`      // 胎神占方
+	Chongsha  string `json:"chongsha"`  // 冲煞
+	Nayin     string `json:"nayin"`     // 纳音
+	Dizhi     string `json:"dizhi"`     // 地支
+	Ganzhi    string `json:"ganzhi"`    // 干支
+}
+
+// AuspiciousDemonResp 宜忌神煞API响应结构
+type AuspiciousDemonResp = BaseResp[AuspiciousDemonData]
+
+// AuspiciousDemonData 宜忌神煞数据
+type AuspiciousDemonData struct {
+	Niansansha   string `json:"niansansha"`   // 年三煞
+	Nianqisha    string `json:"nianqisha"`    // 年七煞
+	Niankongwang string `json:"niankongwang"` // 年空亡
+	Yuezhi       string `json:"yuezhi"`       // 月支
+	Yueling      string `json:"yueling"`      // 月令
+	Yuexiang     string `json:"yuexiang"`     // 月相
+	Yuesansha    string `json:"yuesansha"`    // 月三煞
+	Yueqisha     string `json:"yueqisha"`     // 月七煞
+	Yuekongwang  string `json:"yuekongwang"`  // 月空亡
+	Risansha     string `json:"risansha"`     // 日三煞
+	Riqisha      string `json:"riqisha"`      // 日七煞
+	Rikongwang   string `json:"rikongwang"`   // 日空亡
+	Tjjs         string `json:"tjjs"`         // 推荐吉时
+	Taisuiwei    string `json:"taisuiwei"`    // 太岁位
+	Fantaisui    string `json:"fantaisui"`    // 犯太岁
+	Esbx         string `json:"esbx"`         // 二十八宿
+	Jiuxing      string `json:"jiuxing"`      // 九星
+	Rilu         string `json:"rilu"`         // 日禄
+	Zhongdong    string `json:"zhongdong"`    // 仲冬
+	Suipowei     string `json:"suipowei"`     // 岁破位
+	Niantaisui   string `json:"niantaisui"`   // 年太岁
+	Caishen      string `json:"caishen"`      // 财神
+	Xishen       string `json:"xishen"`       // 喜神
+	Yangguishen  string `json:"yangguishen"`  // 阳贵神
+	Yinguishen   string `json:"yinguishen"`   // 阴贵神
+	Fushen       string `json:"fushen"`       // 福神
+	Yjgx         string `json:"yjgx"`         // 易经卦象
+	Wuhou        string `json:"wuhou"`        // 物候
+	Zhishen12    string `json:"zhishen12"`    // 十二值神
+	Zhiri12      string `json:"zhiri12"`      // 十二值日
+	Liuyao       string `json:"liuyao"`       // 六曜
+}
+
+// AuspiciousTimeItem 时辰吉凶数据项
+type AuspiciousTimeItem struct {
+	Shijian   string `json:"shijian"`   // 时间，格式：HH:MM:SS-HH:MM:SS
+	Jixiong   string `json:"jixiong"`   // 吉凶
+	Jishen    string `json:"jishen"`    // 吉神
+	Xiongshen string `json:"xiongshen"` // 凶神
+	Shichong  string `json:"shichong"`  // 时冲
+	Shizhu    string `json:"shizhu"`    // 时柱
+}
+
+// AuspiciousTimeResp 时辰吉凶API响应结构
+type AuspiciousTimeResp = BaseResp[AuspiciousTimeData]
+
+// AuspiciousTimeData 时辰吉凶数据
+// key 为时辰名称：zi(子), chou(丑), yin(寅), mao(卯), cheng(辰), si(巳), wu(午), wei(未), shen(申), you(酉), xu(戌), hai(亥)
+type AuspiciousTimeData map[string]*AuspiciousTimeItem
+
+// HolidayDetailResp 节假日详情API响应结构
+type HolidayDetailResp = BaseResp[HolidayDetailData]
+
+// HolidayDetailItem 节日详情项
+type HolidayDetailItem struct {
+	Name    string `json:"name"`    // 节日名称
+	Genus   string `json:"genus"`   // 节日种类，public表示公众日或国际日，traditional表示传统节日
+	Day     string `json:"day"`     // 节日公历日期，格式：MMDD
+	LunaDay string `json:"lunaDay"` // 节日的农历日期
+	Info    string `json:"info"`    // 节日的简介
+	Origin  string `json:"origin"`  // 节日的起源
+}
+
+// HolidayDetailData 节假日详情数据
+type HolidayDetailData struct {
+	Day           string              `json:"day"`            // 查询的日期，格式：YYYYMMDD
+	Holiday       string              `json:"holiday"`        // 节日名称，工作日时显示"无"，周末时显示"周末"，节日时显示节日名称
+	Type          string              `json:"type"`           // 类型：1为工作日，2为周末，3为节假日
+	Begin         string              `json:"begin"`          // 节日或周末开始时间，格式：YYYYMMDD，如果是工作日，此字段为空串
+	End           string              `json:"end"`            // 节日或周末结束时间，格式：YYYYMMDD，如果是工作日，此字段为空串
+	HolidayRemark string              `json:"holiday_remark"` // 节日备注
+	WeekDay       int                 `json:"weekDay"`        // 星期几的数字，0-6（0为周日）
+	Cn            string              `json:"cn"`             // 星期几的中文名
+	En            string              `json:"en"`             // 星期几的英文名
+	H             []HolidayDetailItem `json:"h"`              // 节日列表，如果当日没有节日则返回空List，该字段需要请求参数中needDesc上传1才返回
+}
